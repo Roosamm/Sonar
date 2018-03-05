4cdd4b137466a5bfad877b06519b96fc48d8d93c
Daniel Vahla <vahla.daniel@gmail.com>
2018-03-05 14:31:14 +0200

    added more pages and components

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import {User} from '../../app/models/user';
+import {MediaProvider} from '../../providers/media/media';
+import {FrontPage} from '../front/front';
+import {HttpErrorResponse} from '@angular/common/http';
 
 /**
  * Generated class for the LoginPage page.
@@ -8,18 +12,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  user: User = {
+    password: '',
+    username: ''
+  };
+
+  status: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public mediaProvider: MediaProvider) {
+  }
+
+  login () {
+    this.mediaProvider.login(this.user).
+    subscribe(response => {
+      console.log(response['token']);
+      localStorage.setItem('token', response['token']);
+      this.navCtrl.setRoot(FrontPage);
+      this.mediaProvider.logged = true;
+    }, (error: HttpErrorResponse) => {
+      console.log(error.error.message);
+      this.status = error.error.message;
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+    if (localStorage.getItem('token') !== null) {
+      this.mediaProvider.getUserData(localStorage.getItem('token')).subscribe(response => {
+        this.navCtrl.setRoot(FrontPage);
+        this.mediaProvider.logged = true;
+      }, (error: HttpErrorResponse) => {
+        console.log(error);
+      });
+    }
   }
 
 }
