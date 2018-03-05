c9008adf5f385fcf2a0c28f013a6b4a976176673
Daniel Vahla <vahla.daniel@gmail.com>
2018-03-05 16:02:43 +0200

    DONT FORGET TO MAKE CHANGES IN APP.ts FILES

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,42 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { MediaProvider } from '../providers/media/media';
+import {FrontPage} from "../pages/front/front";
+import {LoginPage} from "../pages/login/login";
+import {ProfilePage} from "../pages/profile/profile";
+import {UploadPage} from "../pages/upload/upload";
+import {PostPage} from "../pages/post/post";
+import {RegisterPage} from "../pages/register/register";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FrontPage,
+    LoginPage,
+    ProfilePage,
+    UploadPage,
+    PostPage,
+    RegisterPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
+    FormsModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    FrontPage,
+    LoginPage,
+    ProfilePage,
+    UploadPage,
+    PostPage,
+    RegisterPage
   ],
   providers: [
     StatusBar,
