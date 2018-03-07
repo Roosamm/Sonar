7aa9776e235c13ac3d28263838f41781f18458e0
Kseniia Chumachenko <timeofneon@gmail.com>
2018-03-07 21:31:13 +0200

    Unchecked (ionic bug) page layout (top menu, cards, no pipes)

--- src/pages/front/front.ts
+++ src/pages/front/front.ts
@@ -16,11 +16,36 @@ import {MediaProvider} from '../../providers/media/media';
 })
 export class FrontPage {
 
+  searchQuery: string = '';
+  items: string[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public mediaProvider: MediaProvider) {
+    this.initializeItems();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FrontPage');
+  initializeItems() {
+    this.items = [
+      'culture',
+      'sport',
+      'science',
+      'food',
+      'business',
+      'animals'
+    ];
   }
 
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
 }
