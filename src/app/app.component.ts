930f57bf80e8d4a56510fc794ef0c5a2b14360f4
Kseniia Chumachenko <timeofneon@gmail.com>
2018-03-10 12:44:44 +0200

    Checked front page layout (menu, cards, no pipes)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,47 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component, ViewChild} from '@angular/core';
+import {Nav, Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
 import {FrontPage} from "../pages/front/front";
-import {UploadPage} from "../pages/upload/upload";
-import {RegisterPage} from "../pages/register/register";
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
   rootPage:any = FrontPage;
 
+  pages: Array<{ title: string, component: any }>;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      {title: 'Home', component: FrontPage},
+      {title: 'Culture', component: FrontPage},
+      {title: 'Sport', component: FrontPage},
+      {title: 'Science', component: FrontPage},
+      {title: 'Food', component: FrontPage},
+      {title: 'Business', component: FrontPage},
+      {title: 'Animals', component: FrontPage}
+    ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
 }
 
