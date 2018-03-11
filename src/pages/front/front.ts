23ebb3af34d1dfadacccbe02007ef3f3fae939fd
Daniel Vahla <vahla.daniel@gmail.com>
2018-03-11 10:38:54 +0200

    front page with feed

--- src/pages/front/front.ts
+++ src/pages/front/front.ts
@@ -17,15 +17,20 @@ import {LoginPage} from '../login/login';
 })
 export class FrontPage {
 
-  searchQuery: string = '';
   items: string[];
   mediaArray: Array<string>;
   sonarArray: Array<string>;
   resultArray: Array<string>;
+  eventArray: Array<string>;
+  postArray: Array<string>;
+  allEvents: Array<string>;
+  allPosts: Array<string>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public mediaProvider: MediaProvider,  public menu: MenuController) {
     this.initializeItems();
     menu.enable(true);
+    this.getEventFeed();
+    this.getPostFeed();
   }
 
   initializeItems() {
@@ -38,16 +43,56 @@ export class FrontPage {
       'animals'
     ];
   }
-
+//final search results are in eventArray and postArray
   getItems(event) {
-    this.resultArray = [];
+    //empty data holders and fetch new stuff
+    this.resultArray = this.sonarArray = this.mediaArray = this.eventArray = this.postArray = this.allPosts = this.allEvents = [];
     this.mediaProvider.getMediaByTag(event).subscribe(data =>(this.mediaArray = data));
     this.mediaProvider.getMediaByTag("Sonar").subscribe(stuff=>(this.sonarArray = stuff));
 
     for(let str in this.mediaArray){
       for(let tmp in this.sonarArray){
-        if(str['tag'].equals(tmp['tag'])){
+        if(str['filename'].equals(tmp['filename'])){
           this.resultArray.push(tmp);
+        }
+      }
+    }
+
+    this.mediaProvider.getMediaByTag("event").subscribe(data=>(this.allEvents = data));
+    this.mediaProvider.getMediaByTag("post").subscribe(data =>(this.postArray = data));
+
+    for(let sonar in this.sonarArray){
+      for(let evt in this.allEvents){
+        if(sonar['filename'].equals(evt['filename'])){
+          this.eventArray.push(evt)
+        }
+      }
+      for(let post in this.allPosts){
+        if(sonar['filename'].equals(post['filename'])){
+          this.postArray.push(post)
+        }
+      }
+    }
+  }
+
+  getEventFeed(){
+    this.mediaProvider.getMediaByTag("Sonar").subscribe(data =>(this.sonarArray = data));
+    this.mediaProvider.getMediaByTag("event").subscribe(data =>(this.allEvents = data));
+    for(let sonar in this.sonarArray){
+      for(let evt in this.allEvents){
+        if(sonar['filename'].equals(evt['filename'])){
+          this.eventArray.push(evt);
+        }
+      }
+    }
+  }
+  getPostFeed(){
+    this.mediaProvider.getMediaByTag("Sonar").subscribe(data =>(this.sonarArray = data));
+    this.mediaProvider.getMediaByTag("post").subscribe(data =>(this.allPosts = data));
+    for(let sonar in this.sonarArray){
+      for(let post in this.allPosts){
+        if(sonar['filename'].equals(post['filename'])){
+          this.eventArray.push(post);
         }
       }
     }
